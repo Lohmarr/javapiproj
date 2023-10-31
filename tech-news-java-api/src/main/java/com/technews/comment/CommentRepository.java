ed8cb115f5aae69a8815d628b2bca1462c8ab19d
Lohmarr <119974080+Lohmarr@users.noreply.github.com>
2023-10-31 14:19:20 -0400

    Delete tech-news-java-api directory

--- tech-news-java-api/src/main/java/com/technews/comment/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.technews.comment;
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    List<Comment> findAllCommentsByPostId(int postId);
-}
--- tech-news-java-api/src/main/java/com/technews/user/Role.java
+++
@@ -1,6 +0,0 @@
-package com.technews.user;
-
-public enum Role {
-    USER,
-    ADMIN
-}
